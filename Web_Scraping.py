156666afc4339d195d09e51e6e69665449866f3a
Cyrus-Yeung <145176209+Cyrus-Yeung@users.noreply.github.com>
2024-04-02 16:17:12 +0800

    Updated Web_Scraping.py

--- Web_Scraping.py
+++ Web_Scraping.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import Keys, ActionChains
 from time import sleep
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -12,7 +13,7 @@ searchLink = f"https://www.cnbc.com/search/?query={targetAsset}&qsearchterm={tar
 
 driver = webdriver.Chrome()
 driver.get(searchLink)
-sleep(10) # wait for page to completely load
+sleep(5) # wait for page to completely load
 try:
     driver.execute_script("document.getElementById('formatfilter').value = 'Articles'") # articles only
     driver.execute_script("var evnt = new Event('change'); document.getElementById('formatfilter').dispatchEvent(evnt)")
@@ -20,12 +21,14 @@ try:
 except:
     print(f"No search result for asset {targetAsset}")
     quit()
-sleep(5) # wait for filter to finish
-allSearchResult = driver.find_elements(By.CSS_SELECTOR, ".resultlink") # news articles are of class "resultlink"
+sleep(1) # wait for filter to finish
+ActionChains(driver).key_down(Keys.END) # scrolll to bottom
+sleep(5) # wait for result to load
+allSearchResult = driver.find_elements(By.CSS_SELECTOR, "div.SearchResult-searchResultCard>a.resultlink") # news articles are of class "resultlink"
 try:
     allSearchResult = [searchResult.get_attribute("href") for searchResult in allSearchResult] # extract url from search results
 except:
-    allSearchResult = driver.find_elements(By.CSS_SELECTOR, ".resultlink")
+    allSearchResult = driver.find_elements(By.CSS_SELECTOR, "div.SearchResult-searchResultCard>a.resultlink")
     allSearchResult = [searchResult.get_attribute("href") for searchResult in allSearchResult]
 
 articles = [] # set of new articles, each item is a string containing all text in an article
@@ -65,6 +68,5 @@ for searchResult in allSearchResult:
 
 tokenizedCorpus = list(map(lambda doc: word_tokenize(doc), articles)) # tokenize each document
 processedCorpus = [list(filter(lambda word: word not in stopwords.words("english") and word.isalpha(), doc)) for doc in tokenizedCorpus]
-
 finalizedCorpus = list(zip(articleDate, processedCorpus))
 print(finalizedCorpus[0])
