836a8e31232f83276b4f07eb0e98426f9ca5f205
Cyrus-Yeung <145176209+Cyrus-Yeung@users.noreply.github.com>
2024-04-05 15:16:33 +0800

    Added Python file for Sentiment Analysis

---
+++ Sentiment_Analysis.py
@@ -0,0 +1,16 @@
+from pickle import load
+from textblob import TextBlob
+
+with open("newsArticlesCorpus.pickle", "rb") as corpusData:
+    corpusData = load(corpusData)
+
+for i in range(len(corpusData)):
+    articleSentiment = TextBlob(" ".join(corpusData[i][2]))
+    if articleSentiment.polarity > 0:
+        label = "bullish"
+    else:
+        label = "bearish"
+    corpusData[i] = corpusData[i] + (articleSentiment.polarity, label)
+
+for i in corpusData:
+    print(i[0], i[1], i[2][1:10], i[3], i[4])
