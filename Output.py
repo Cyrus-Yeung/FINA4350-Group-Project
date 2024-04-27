40eb86809c7daefd8ea11df0371903d9433095ad
Cyrus-Yeung <145176209+Cyrus-Yeung@users.noreply.github.com>
2024-04-27 23:31:20 +0800

    Added the class definition of sentimentScore for streaming Sentiment_Labels.csv

--- Output.py
+++ Output.py
@@ -1,1 +1,24 @@
+from csv import reader
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+class sentimentScore:
+    def __init__(self,  filename):
+        self.filename = filename
+    def __iter__(self): # create a generator function for streaming data
+        with open(self.filename, "r", newline = "") as sentimentInput:
+            csvreader = reader(sentimentInput)
+            isHeader = True # indicates whether the row is the header
+            for row in csvreader:
+                if isHeader:
+                    isHeader = False
+                    continue # skip headers
+                row[0] = datetime.strptime(row[0], "%b %d %Y") # convert date of publication to date format
+                row[2] = float(row[2]) # convert polarity scores to floating-point numbers
+                yield row
+
+# Obtain the gold price from Yahoo Finance
+driver = webdriver.Chrome()
+driver.get("https://finance.yahoo.com/quote/GC%3DF/history?" + \
+           "period1=1679011200&period2=1712275200")
